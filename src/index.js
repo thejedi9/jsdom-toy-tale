cb0e09e57bdb7dae238a7810f2db5c7954315924
Adam Schwartz <adamberard24@gmail.com>
2021-03-24 11:02:28 -0400

    Switching roles

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
+    fetch("http://localhost:3000/toys")
+    .then(res => res.json())
+    .then(function(toyCollection){
+      toyCollection.forEach(function(toyObjectParameter){
+        turnToyObjToLi(toyObjectParameter)
+      })
+    })
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
