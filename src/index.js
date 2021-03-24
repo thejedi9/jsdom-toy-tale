adb9309053513cfb17ea5af8310f14b48475fde3
Jede <brahmajede@gmail.com>
2021-03-24 12:02:37 -0400

    attempted effort

--- src/index.js
+++ src/index.js
@@ -4,13 +4,32 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
+   
+   // this is AnJ start
+   let toyListOl = document.querySelector("ol#toy-collection")
+
     fetch("http://localhost:3000/toys")
     .then(res => res.json())
     .then(function(toyCollection){
-      toyCollection.forEach(function(toyObjectParameter){
-        turnToyObjToLi(toyObjectParameter)
+      toyCollection.forEach(function(toyObject){
+        turnToyObjToLi(toyObject)
       })
-    })
+    function turnToyObjToLi(toyObj){
+ 
+    let toyCardLi = document.createElement("div")
+    toyCardLi.classList.add("card")
+
+    let toyName = document.createElement("h2")
+    toyName.innerText = `${toyObj.name}`
+
+    let toyImage = document.createElement("Img")
+    toyImage.innerText = `${toyObj.image}`
+
+    let toyLikesP = document.createElement("P")
+    toyLikesP.innerText = `${toyObj.likes} Likes`
+
+    }
+    
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
